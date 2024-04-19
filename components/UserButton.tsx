1ea143f39e5459a9196a948e5c6e445aaf955a1f
Xuong Huynh <longxuongz@gmail.com>
2024-04-20 01:36:43 +0700

    Change user icon if user is logged in

--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -1,12 +1,15 @@
 import { Button } from "@/components/ui/button";
-import { User } from "lucide-react";
+import { getServerCurrentUser } from "@/lib/auth";
+import { User, UserCheck } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 
-const UserButton = () => {
+const UserButton = async() => {
+    const currentUser = await getServerCurrentUser();
     return (
         <Link href="/login" className="hover:bg-transparent hover:text-amber-900 p-0">
-            <User className="h-[22px] w-[22px]" />
+            {currentUser ? <UserCheck className="h-[22px] w-[22px]" /> : <User className="h-[22px] w-[22px]" />}
+            
         </Link>
     );
 };
